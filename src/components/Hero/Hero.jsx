0cf10a190611efd883121d7b1994b5b6771f29a5
s42121191-a11y <s42121191@gmail.com>
2026-07-05 17:23:19 +0530

    main front page  & navbar finished(css style)

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,19 @@ import './Hero.css'
 function Hero() {
    return(
     <>
+   
     <div className='h_container'>
-        <h2 className='h_title'>Welcome TO Our Delicious Corrner</h2>
+
+        <h3 className='h_head'> Welcome To Our Delicious Corrner</h3>
+        <span className='h_title'>The Finest Dishes</span>
+        <p className='h_decs'>we have proper passion for cooking and love is the secret ingredient that makes all our dishes test better and magical</p>
+
+    </div>
+
+    <div className='btn_container'>
+
+    <button className='explore_btn'>Explore Menu</button>
+    <button className='order_btn'>Order Now</button>
 
     </div>
     </>
