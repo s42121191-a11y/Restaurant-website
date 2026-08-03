8ba9d474c53b9b558ec3e91282ce252322d76c19
s42121191-a11y <s42121191@gmail.com>
2026-08-03 12:49:40 +0530

    Fix build

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero/Hero.jsx";
 import About from "./components/About/About.jsx"
 import MenuCard from "./components/Menu/MenuCard.jsx";
 import Reservation from './components/Reservation/Reservation.jsx';
-import Testimonial from './components/Testimonials/Testimonial.jsx';
+import Testimonial from "./components/Testimonial/Testimonial.jsx";
 import Footer from './components/Footer/Footer.jsx';
 
 import { useEffect } from "react";
